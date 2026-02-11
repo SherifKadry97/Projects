1cb2b03a42ab846d9bfde635664f59b81ecc6e54
Sherif Kadry <40645685+SherifKadry97@users.noreply.github.com>
2026-02-12 00:21:35 +0200

    Remove key variables
    Removed key variables.

--- terraform-aws-2tier-infra/variables.tf
+++ terraform-aws-2tier-infra/variables.tf
@@ -3,14 +3,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "aws_access_key" {
-  default = "XXXXXXXXXXXXXXXXX"
-}
-
-variable "aws_secret_key" {
-  default = "XXXXXXXXXXXXXXXXXXXXXXXXX"
-}
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
